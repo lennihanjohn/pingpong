77cf0b253cdec66ef7c95e754f01c20ee4cee89d
linhan <linhan@smartbuildingapps.com>
2019-06-24 21:31:58 -0400

    finish 2 and 3

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -33,9 +33,53 @@ app.controller("scoreboardController", ['$scope', function($scope) {
     $scope.league = [];
 
     $scope.addResult = function(result) {
+        $scope.submitted = true;
         result.id = $scope.results.length + 1;
+        $scope.comparePlayersScore(result)
+
         $scope.results.push(result);
-        $scope.result = {};
+        $scope.result = {};        
     }
 
+    $scope.comparePlayersScore = function (result) {       
+        console.log("checkPlayersScore");
+
+        if (result.score_1 < 0 || result.score_2 < 0){
+            console.log("wrong input");
+        }
+        else {
+            if (result.score_1 == 11 && result.score_2 < 10) { 
+                console.log(result.player_1 + " wins");
+            }
+            if (result.score_2 == 11 && result.score_1 < 10) { 
+                console.log(result.player_2 + " wins");
+            }
+            if (result.score_1 < 11 && result.score_2 < 11) { 
+                console.log("in progressing"); 
+            } 
+            if (result.score_1 > 10 && result.score_2 > 10) {
+                if ((result.score_1 - result.score_2) > 2 || (result.score_2 - result.score_1) > 2) {
+                    console.log("wrong input");
+                }
+                if ((result.score_1 - result.score_2) == 2) { 
+                    console.log(result.player_1 + " wins");
+                }
+                if ((result.score_2 - result.score_1) == 2) { 
+                    console.log(result.player_2 + " wins");
+                }
+                if ((result.score_1 - result.score_2) <= 1 || (result.score_2 - result.score_1) <= 1) {
+                    console.log("in progressing");
+                } 
+            }
+        }
+    };
+    $scope.addPlayer = function() {
+        if (angular.isDefined($scope.name) && $scope.name != '') {
+            // ADD A NEW ELEMENT.
+            $scope.players.push({ id: $scope.players.length + 1,  name: $scope.name });
+            // CLEAR THE FIELDS.
+            $scope.name = '';
+        }
+
+    }
 }]);
